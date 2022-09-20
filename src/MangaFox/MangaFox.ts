752a6a24ed9eaaeffe63f10180e7347a9cedce69
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2022-09-20 23:10:30 +0200

    Add MangaFox

---
+++ src/MangaFox/MangaFox.ts
@@ -0,0 +1,194 @@
+import {
+    Source,
+    SourceManga,
+    Chapter,
+    ChapterDetails,
+    HomeSection,
+    SearchRequest,
+    PagedResults,
+    SourceInfo,
+    BadgeColor,
+    TagSection,
+    ContentRating,
+    Request,
+    Response
+} from '@paperback/types'
+
+import {
+    isLastPage,
+    parseTags,
+    parseChapterDetails,
+    parseChapters,
+    parseHomeSections,
+    parseMangaDetails,
+    parseSearch,
+    parseViewMore
+} from './MangaFoxParser'
+
+import { URLBuilder } from './MangaFoxHelper'
+
+const FF_DOMAIN = 'https://fanfox.net'
+const FF_DOMAIN_MOBILE = 'https://m.fanfox.net'
+
+export const MangaFoxInfo: SourceInfo = {
+    version: '3.0.0',
+    name: 'MangaFox',
+    icon: 'icon.png',
+    author: 'Netsky',
+    authorWebsite: 'https://github.com/TheNetsky',
+    description: 'Extension that pulls manga from MangaHere.',
+    contentRating: ContentRating.MATURE,
+    websiteBaseURL: FF_DOMAIN,
+    sourceTags: [
+        {
+            text: 'Notifications',
+            type: BadgeColor.GREEN
+        }
+    ]
+}
+export class MangaFox extends Source {
+
+    readonly cookies = [App.createCookie({ name: 'isAdult', value: '1', domain: 'fanfox.net' })];
+
+    requestManager = App.createRequestManager({
+        requestsPerSecond: 5,
+        requestTimeout: 20000,
+        interceptor: {
+            interceptRequest: async (request: Request): Promise<Request> => {
+                request.headers = {
+                    ...(request.headers ?? {}),
+                    ...{
+                        'referer': `${FF_DOMAIN}/`,
+                        'user-agent': await this.requestManager.getDefaultUserAgent()
+                    }
+                }
+                return request
+            },
+            interceptResponse: async (response: Response): Promise<Response> => {
+                return response
+            }
+        }
+    });
+
+    override getMangaShareUrl(mangaId: string): string { return `${FF_DOMAIN}/manga/${mangaId}` }
+
+    async getMangaDetails(mangaId: string): Promise<SourceManga> {
+        const request = App.createRequest({
+            url: `${FF_DOMAIN}/manga/`,
+            method: 'GET',
+            param: mangaId
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data)
+        return parseMangaDetails($, mangaId)
+    }
+
+    async getChapters(mangaId: string): Promise<Chapter[]> {
+        const request = App.createRequest({
+            url: `${FF_DOMAIN}/manga/`,
+            method: 'GET',
+            param: mangaId,
+            cookies: this.cookies
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data)
+        return parseChapters($)
+    }
+
+    async getChapterDetails(mangaId: string, chapterId: string): Promise<ChapterDetails> {
+        const request = App.createRequest({
+            url: `${FF_DOMAIN_MOBILE}/roll_manga/${mangaId}/${chapterId}`,
+            method: 'GET',
+            cookies: this.cookies
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data)
+        return parseChapterDetails($, mangaId, chapterId)
+    }
+
+    override async getHomePageSections(sectionCallback: (section: HomeSection) => void): Promise<void> {
+        const request = App.createRequest({
+            url: FF_DOMAIN,
+            method: 'GET'
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data)
+        parseHomeSections($, sectionCallback)
+    }
+
+    override async getViewMoreItems(homepageSectionId: string, metadata: any): Promise<PagedResults> {
+        const page: number = metadata?.page ?? 1
+        let param = ''
+
+        switch (homepageSectionId) {
+            case 'hot_release':
+                param = '/hot/'
+                break
+            case 'new_manga':
+                param = `/directory/${page}.html?news`
+                break
+            case 'latest_updates':
+                param = `/releases/${page}.html`
+                break
+            default:
+                throw new Error(`Invalid homeSectionId | ${homepageSectionId}`)
+        }
+
+        const request = App.createRequest({
+            url: `${FF_DOMAIN}/`,
+            method: 'GET',
+            param
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data)
+        const manga = parseViewMore($, homepageSectionId)
+
+        metadata = !isLastPage($) ? { page: page + 1 } : undefined
+        return App.createPagedResults({
+            results: manga,
+            metadata
+        })
+    }
+
+    async getSearchResults(query: SearchRequest, metadata: any): Promise<PagedResults> {
+        const page: number = metadata?.page ?? 1
+
+        const url = new URLBuilder(FF_DOMAIN)
+            .addPathComponent('search')
+            .addQueryParameter('page', page)
+            .addQueryParameter('title', encodeURI(query?.title || ''))
+            .addQueryParameter('genres', query.includedTags?.map((x: any) => x.id).join('%2C'))
+            .buildUrl()
+
+        const request = App.createRequest({
+            url: url,
+            method: 'GET'
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data)
+        const manga = parseSearch($)
+
+        metadata = !isLastPage($) ? { page: page + 1 } : undefined
+        return App.createPagedResults({
+            results: manga,
+            metadata
+        })
+    }
+
+    override async getTags(): Promise<TagSection[]> {
+        const request = App.createRequest({
+            url: `${FF_DOMAIN}/search?`,
+            method: 'GET'
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data)
+        return parseTags($)
+    }
+}

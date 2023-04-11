9f0bfd8b2d2519bf2211f62ae28d0ec691212d16
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2023-04-11 17:38:44 +0200

    Add Mitaku

---
+++ src/Mitaku/Mitaku.ts
@@ -0,0 +1,269 @@
+import {
+    SourceManga,
+    Chapter,
+    ChapterDetails,
+    HomeSection,
+    SearchRequest,
+    PagedResults,
+    SourceInfo,
+    ContentRating,
+    BadgeColor,
+    Request,
+    Response,
+    TagSection,
+    SourceIntents,
+    HomeSectionType,
+    ChapterProviding,
+    MangaProviding,
+    Searchable,
+    Tag
+} from '@paperback/types'
+
+import {
+    parseChapterDetails,
+    parseChapters,
+    parseHomeSections,
+    parseMangaDetails,
+    isLastPage
+} from './MitakuParser'
+
+const MT_DOMAIN = 'https://mitaku.net'
+
+export const MitakuInfo: SourceInfo = {
+    version: '1.0.0',
+    name: 'Mitaku',
+    icon: 'icon.png',
+    author: 'Netsky',
+    authorWebsite: 'https://github.com/TheNetsky',
+    description: 'Extension that pulls manga from Mitaku.net',
+    contentRating: ContentRating.ADULT,
+    websiteBaseURL: MT_DOMAIN,
+    sourceTags: [
+        {
+            text: '18+',
+            type: BadgeColor.YELLOW
+        }
+    ],
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED
+}
+
+export class Mitaku implements Searchable, MangaProviding, ChapterProviding {
+
+    constructor(private cheerio: CheerioAPI) { }
+
+    requestManager = App.createRequestManager({
+        requestsPerSecond: 4,
+        requestTimeout: 15000,
+        interceptor: {
+            interceptRequest: async (request: Request): Promise<Request> => {
+                request.headers = {
+                    ...(request.headers ?? {}),
+                    ...{
+                        'referer': `${MT_DOMAIN}/`,
+                        'user-agent': await this.requestManager.getDefaultUserAgent()
+                    }
+                }
+                return request
+            },
+            interceptResponse: async (response: Response): Promise<Response> => {
+                return response
+            }
+        }
+    });
+
+    getMangaShareUrl(mangaId: string): string { return `${MT_DOMAIN}/?p=${mangaId}` }
+
+    async getMangaDetails(mangaId: string): Promise<SourceManga> {
+        const request = App.createRequest({
+            url: `${MT_DOMAIN}/?p=${mangaId}`,
+            method: 'GET'
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        this.CloudFlareError(response.status)
+        const $ = this.cheerio.load(response.data as string)
+        return parseMangaDetails($, mangaId)
+    }
+
+    async getChapters(mangaId: string): Promise<Chapter[]> {
+        return parseChapters(mangaId)
+    }
+
+    async getChapterDetails(mangaId: string, chapterId: string): Promise<ChapterDetails> {
+        const request = App.createRequest({
+            url: `${MT_DOMAIN}/?p=${chapterId}`,
+            method: 'GET'
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        this.CloudFlareError(response.status)
+        const $ = this.cheerio.load(response.data as string)
+        return parseChapterDetails($, mangaId, chapterId)
+    }
+
+    async getHomePageSections(sectionCallback: (section: HomeSection) => void): Promise<void> {
+        const sections = [
+            {
+                request: App.createRequest({
+                    url: `${MT_DOMAIN}/category/ero-cosplay`,
+                    method: 'GET'
+                }),
+                sectionID: App.createHomeSection({
+                    id: 'cosplay',
+                    title: 'Cosplay Galleries',
+                    containsMoreItems: true,
+                    type: HomeSectionType.singleRowNormal
+                }),
+            },
+            {
+                request: App.createRequest({
+                    url: `${MT_DOMAIN}/category/nude`,
+                    method: 'GET'
+                }),
+                sectionID: App.createHomeSection({
+                    id: 'nude',
+                    title: 'Nude Galleries',
+                    containsMoreItems: true,
+                    type: HomeSectionType.singleRowNormal
+                })
+            },
+            {
+                request: App.createRequest({
+                    url: `${MT_DOMAIN}/category/sexy-set`,
+                    method: 'GET'
+                }),
+                sectionID: App.createHomeSection({
+                    id: 'sexy-set',
+                    title: 'Sexy Set Galleries',
+                    containsMoreItems: true,
+                    type: HomeSectionType.singleRowNormal
+                })
+            }
+        ]
+
+        const promises: Promise<void>[] = []
+
+        for (const section of sections) {
+            sectionCallback(section.sectionID)
+            promises.push(
+                this.requestManager.schedule(section.request, 1)
+                    .then(response => {
+                        this.CloudFlareError(response.status)
+                        const $ = this.cheerio.load(response.data as string)
+                        const items = parseHomeSections($)
+                        section.sectionID.items = items
+                        sectionCallback(section.sectionID)
+                    })
+            )
+        }
+
+        await Promise.all(promises)
+    }
+
+    async getViewMoreItems(homepageSectionId: string, metadata: any): Promise<PagedResults> {
+        if (metadata?.completed) return metadata
+
+        const page: number = metadata?.page ?? 1
+        let param = ''
+
+        switch (homepageSectionId) {
+            case 'cosplay':
+                param = `/category/ero-cosplay/page/${page}`
+                break
+            case 'nude':
+                param = `/category/nude/page/${page}`
+                break
+            case 'sexy-set':
+                param = `/category/sexy-set/page/${page}`
+                break
+            default:
+                throw new Error('Requested to getViewMoreItems for a section ID which doesn\'t exist')
+        }
+
+        const request = App.createRequest({
+            url: MT_DOMAIN + param,
+            method: 'GET'
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        this.CloudFlareError(response.status)
+        const $ = this.cheerio.load(response.data as string)
+        const manga = parseHomeSections($)
+
+        metadata = !isLastPage($) ? { page: page + 1 } : undefined
+        return App.createPagedResults({
+            results: manga,
+            metadata
+        })
+    }
+
+    async getSearchTags(): Promise<TagSection[]> {
+        const request = App.createRequest({
+            url: `${MT_DOMAIN}/wp-json/wp/v2/tags?per_page=100`,
+            method: 'GET'
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const tagJSON = JSON.parse(response.data as string)
+
+        const arrayTags: Tag[] = []
+
+        for (const tag of tagJSON) {
+            const label = tag.name
+            const id = tag.slug
+
+            if (!id || !label) continue
+            arrayTags.push({ id: id, label: label })
+        }
+
+        const tagSections: TagSection[] = [App.createTagSection({ id: '0', label: 'genres', tags: arrayTags.map(x => App.createTag(x)) })]
+        return tagSections
+    }
+
+    async getSearchResults(query: SearchRequest, metadata: any): Promise<PagedResults> {
+        const page: number = metadata?.page ?? 1
+
+        let request
+        // Regular search
+        if (query.title) {
+            request = App.createRequest({
+                url: `${MT_DOMAIN}/page/${page}?s=${encodeURI(query.title)}`,
+                method: 'GET'
+            })
+
+            // Tag Search
+        } else {
+            request = App.createRequest({
+                url: `${MT_DOMAIN}/tag/${query?.includedTags?.map((x: Tag) => x.id)[0]}/page/${page}`,
+                method: 'GET'
+            })
+        }
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data as string)
+        const manga = parseHomeSections($)
+
+        metadata = !isLastPage($) ? { page: page + 1 } : undefined
+        return App.createPagedResults({
+            results: manga,
+            metadata
+        })
+    }
+
+    CloudFlareError(status: number): void {
+        if (status > 400) {
+            throw new Error(`CLOUDFLARE BYPASS ERROR:\nPlease go to the homepage of <${Mitaku.name}> and press the cloud icon.`)
+        }
+    }
+
+    async getCloudflareBypassRequestAsync(): Promise<Request> {
+        return App.createRequest({
+            url: MT_DOMAIN,
+            method: 'GET',
+            headers: {
+                'referer': `${MT_DOMAIN}/`,
+                'user-agent': await this.requestManager.getDefaultUserAgent()
+            }
+        })
+    }
+}
---
+++ src/Mitaku/MitakuParser.ts
@@ -0,0 +1,107 @@
+import {
+    Chapter,
+    ChapterDetails,
+    Tag,
+    SourceManga,
+    PartialSourceManga,
+    TagSection
+} from '@paperback/types'
+
+import entities = require('entities')
+
+export const parseMangaDetails = ($: CheerioStatic, mangaId: string): SourceManga => {
+    const images = $('img', 'div.msacwl-slider-wrap')
+
+    const title = decodeHTMLEntity($('h1.entry-title').first().text().trim())
+    const artist = decodeHTMLEntity($('p:contains(Cosplayer:)').text().trim().replace('Cosplayer:', '').trim())
+    const description = `Cosplayer: ${artist}\n\nGallery: ${title}\n\nImages: ${images.length}`
+    const image = images.first().attr('data-src') ?? ''
+
+
+    const arrayTags: Tag[] = []
+    for (const tag of $('a', 'span.tag-links').toArray()) {
+        const id = $(tag).attr('href')?.split('/tag/')[1]?.replace(/\//g, '')
+        const label = $(tag).text().trim()
+        if (!id || !label) continue
+        arrayTags.push({ id: id, label: label })
+    }
+    const tagSections: TagSection[] = [App.createTagSection({ id: '0', label: 'genres', tags: arrayTags.map(x => App.createTag(x)) })]
+
+    return App.createSourceManga({
+        id: mangaId,
+        mangaInfo: App.createMangaInfo({
+            titles: [title],
+            artist: artist,
+            author: artist,
+            status: 'Completed',
+            image: encodeURI(image),
+            desc: description,
+            tags: tagSections
+        })
+    })
+}
+
+export const parseChapters = (mangaId: string): Chapter[] => {
+    return [App.createChapter({
+        id: mangaId,
+        name: 'Gallery',
+        langCode: 'ENG',
+        chapNum: 1,
+        volume: 0
+    })]
+}
+
+export const parseChapterDetails = ($: CheerioStatic, mangaId: string, chapterId: string): ChapterDetails => {
+    const pages: string[] = []
+
+    // Remove last image since it's the telegram icon
+    for (const img of $('img', 'div.msacwl-slider-wrap').toArray()) {
+        let image = $(img).attr('data-src') ?? ''
+        if (!image) image = $(img).attr('data-mfp-src') ?? ''
+        if (!image) continue
+        pages.push(encodeURI(image))
+    }
+
+    const chapterDetails = App.createChapterDetails({
+        id: chapterId,
+        mangaId: mangaId,
+        pages: pages
+    })
+    return chapterDetails
+}
+
+export const parseHomeSections = ($: CheerioStatic): PartialSourceManga[] => {
+    const collectedIds: string[] = []
+    const itemArray: PartialSourceManga[] = []
+
+    for (const item of $('article', 'div#content').toArray()) {
+        const postId = $(item).attr('id')
+        const id = postId?.split('post-').pop()
+        const image: string = $('img', item).first().attr('data-src') ?? ''
+        const title: string = $('h2.entry-title', item).text().trim()
+        const subtitle: string = $('span.tag-links > a', item).toArray().map(x => $(x).text().trim()).join(', ')
+
+        if (!id || isNaN(Number(id)) || !title || collectedIds.includes(id)) continue
+        itemArray.push(App.createPartialSourceManga({
+            image: encodeURI(image),
+            title: decodeHTMLEntity(title),
+            mangaId: id,
+            subtitle: decodeHTMLEntity(subtitle)
+        }))
+        collectedIds.push(id)
+    }
+
+    return itemArray
+}
+
+const decodeHTMLEntity = (str: string): string => {
+    return entities.decodeHTML(str)
+}
+
+export const isLastPage = ($: CheerioStatic): boolean => {
+    let isLast = true
+    const hasNext = Boolean($('a.last', 'div.wp-pagenavi').first())
+
+    if (hasNext) isLast = false
+    return isLast
+}
